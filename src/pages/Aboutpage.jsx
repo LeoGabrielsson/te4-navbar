3ab51201a845e95615016e10c58e466d782ea343
= <=>
2023-09-14 15:11:31 +0200

    🎉

---
+++ src/pages/Aboutpage.jsx
@@ -0,0 +1,9 @@
+function Aboutpage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+
+export default Aboutpage
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+function Homepage() {
+    return (
+        <main className='container section flow'>
+            <h1>Hello</h1>
+            <h2>te</h2>
+            <h3>fe</h3>
+            <p>Lorem ipsum dolor et amet</p>
+        </main>
+    )
+}
+
+export default Homepage
